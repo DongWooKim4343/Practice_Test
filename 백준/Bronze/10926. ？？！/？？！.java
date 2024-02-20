55223e0a6aca0e899d2be3198da724f68792162c
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2024-02-20 18:52:57 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String emo ="??!";
+        String id = input.nextLine();
+        
+
+        System.out.println(id+emo);
+        input.close();
+        }
+        
+}
