0e8fa4f8c58f88477b91118e9bdba95de1ce6e64
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2024-02-20 18:57:41 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        int year = input.nextInt();
+        System.out.println(year-543);
+    }
+}
