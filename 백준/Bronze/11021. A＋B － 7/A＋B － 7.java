2c36b8ef362810d91684988936b11b49287369cd
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2024-03-05 01:49:04 +0900

    [Bronze V] Title: A+B - 7, Time: 160 ms, Memory: 16120 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(input.readLine());
+
+        for(int i = 0; i<num; i++){
+            StringTokenizer st = new StringTokenizer(input.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int sum = a+b;
+
+            output.write("Case #"+(i+1)+": "+(sum)+"\n");
+        }
+        output.flush();
+        output.close();
+    }
+}
