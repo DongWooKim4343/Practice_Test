ab35064a977e945b00edd06bb5af549d630c4d84
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2024-03-05 22:31:07 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(input.readLine());
+
+        for(int i =0;i<count;i++){
+            for(int k=count; k-1>i;k--){
+                output.write(" ");
+            }
+            for(int j=0; j<=i;j++) {
+                output.write("*");
+            }
+            output.newLine();
+        }
+        output.flush();
+        output.close();
+    }
+}
