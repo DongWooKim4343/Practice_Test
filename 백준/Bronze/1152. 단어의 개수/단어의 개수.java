6c4ffce0f6f4c2c7d4cd7e189a404295cbe5ecda
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2024-03-16 01:25:15 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 18496 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(input.readLine()," ");
+
+
+
+        output.write(String.valueOf(st.countTokens()));
+        output.flush();
+
+
+
+
+
+    }
+}
