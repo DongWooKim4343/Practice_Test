12a263be08eaa52904821b242e38a23f31865d17
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2024-03-19 18:06:09 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 136 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+//뭔소리고 그냥 인풋 프린트하면 되는건가
+
+class Main {
+    public static void main(String[] agrs) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word="";
+
+        while((word = input.readLine())!=null) {
+
+            output.write(word+"\n");
+            output.flush();
+        }
+
+
+
+    }
+}
