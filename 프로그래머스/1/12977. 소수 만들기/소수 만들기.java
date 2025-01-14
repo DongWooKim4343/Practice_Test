cf466bb01b429463c254a135d03fd4dcb1d93b96
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-01-14 23:17:48 +0900

    [level 1] Title: 소수 만들기, Time: 115.62 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,9 +1,27 @@
 class Solution {
     public int solution(int[] nums) {
-        int answer = -1;
+        int answer = 0;
+        int sum=0;
+        
+        for(int i = 0 ; i<nums.length; i++){
+            for(int j = i+1 ; j<nums.length; j++){
+                for(int k = j+1 ; k<nums.length; k++){
+                
+                    sum= nums[i]+nums[j]+nums[k];
+                    int check=0;
+                    for(int l = 1; l<=sum; l++){
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
+                        if(sum%l==0){
+                            check++;
+                        }
+                    }
+                    if(check < 3 ){
+                        answer++;
+                    }
+                    }
+            }
+        }
+
 
         return answer;
     }
