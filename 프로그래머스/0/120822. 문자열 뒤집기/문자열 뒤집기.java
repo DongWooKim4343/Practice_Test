4f39e992e48bf368ea0cead18a5ec24fa9623bd5
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-02-06 17:45:32 +0900

    [level 0] Title: 문자열 뒤집기, Time: 13.03 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder answer = new StringBuilder();
+        String answer = "";
         
         for(int i = my_string.length()-1; i>=0; i--){
-            answer.append(my_string.charAt(i));
+            answer+=my_string.charAt(i);
         }
         
-        return answer.toString();
+        return answer;
     }
 }
