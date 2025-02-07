7ffd35d595eeae24781fdd968215ba0a46e8b7ce
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-02-07 17:11:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -5,22 +5,14 @@ class Solution
         //순서대로 토너먼트
         int answer = 0;
         
-         while (a!=b) { 
+        while(a!=b){
+                a =(a+1)/2;
+                b =(b+1)/2;
             
-            if (a % 2 != 0) { // a가 홀수이면
-                a = (a + 1) / 2;  // 다음 라운드 번호 계산
-            } else { // a가 짝수이면
-                a /= 2;
-            }
-            
-            if (b % 2 != 0) { // b가 홀수이면
-                b = (b + 1) / 2;
-            } else { // b가 짝수이면
-                b /= 2;
-            }
-
             answer++;
         }
+
+
 
         return answer;
     }
