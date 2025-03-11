5bb0662fd8a8278cb5dd73238f276be98a501684
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-03-11 18:20:26 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 10.64 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum =0;
+        int mul=1;
+        
+        for(int i=0; i<num_list.length; i++){
+            sum +=num_list[i];
+            mul *=num_list[i];
+        }
+        
+        System.out.println(sum*sum+","+mul);
+        
+        if(sum*sum > mul){
+            answer=1;
+        }
+        
+        return answer;
+    }
+}
