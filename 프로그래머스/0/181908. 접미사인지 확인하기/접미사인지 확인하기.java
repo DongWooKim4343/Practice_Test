550a90ed646f173ddd14bc3604f6c205f372411b
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-03-14 15:55:50 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        
+        for(int i=0; i<my_string.length(); i++){
+            if(is_suffix.equals(my_string.substring(i))){
+                answer=1;
+                break;
+            }else{
+                answer=0;
+            }
+        }
+        
+        return answer;
+    }
+}
