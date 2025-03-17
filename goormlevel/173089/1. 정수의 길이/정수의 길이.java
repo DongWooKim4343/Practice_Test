e62cf2566bee32a6bd1817f25161275fabc0db70
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-03-17 16:41:48 +0900

    [난이도 1] Title: 정수의 길이, Time: 0.06 ms, Memory: 31.37 MB -BaekjoonHub

---
+++ goormlevel/173089/1. 정수의 길이/정수의 길이.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		
+		int num = input.length();
+		
+		System.out.println(num);
+	}
+}
