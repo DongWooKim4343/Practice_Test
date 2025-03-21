c45120c0770e964382e0f1f6a355805870548789
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-03-21 20:57:24 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1404 ms, Memory: 116964 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(input.readLine());
+        int[] arr = new int[num];
+
+        for(int i=0; i<arr.length; i++ ){
+            arr[i]= Integer.parseInt(input.readLine());
+        }
+
+//        for(int i=0; i<arr.length; i++){
+//            for(int j=0; j<arr.length-1; j++){
+//                if(arr[j] > arr[j+1]){
+//                    int temp=arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = temp;
+//                }
+//
+//            }
+//
+//        }
+        
+        Arrays.sort(arr);
+        
+        for(int i=0; i<arr.length; i++){
+            output.write(arr[i]+"\n");
+        }
+        output.flush();
+
+    }
+}
