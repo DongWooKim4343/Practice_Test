70e6f8d550b32e93ea4085fbd21ea9ece0dcddc3
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-03-24 18:17:39 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,21 +1,15 @@
-import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> list = new ArrayList<>();
+        int index = 0;
+        int[] answer = new int[num_list.length];
         
-        for(int i=n; i<num_list.length; i++){
-            list.add(num_list[i]);
-        }        
-        for(int i=0; i<n; i++){
-            list.add(num_list[i]);
-        }
+        for (int i = n;i < num_list.length;i++)
+            answer[index++] = num_list[i];
         
-        int[] answer = new int[list.size()];
+        for (int i = 0;i < n;i++)
+            answer[index++] = num_list[i];
         
-        for(int i=0; i<list.size(); i++){
-            answer[i]=list.get(i);
-        }
         return answer;
     }
 }
