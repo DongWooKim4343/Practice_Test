2c21414d5db5e955ddbffb4cb74f1ffebe35ad35
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-03-31 22:28:43 +0900

    [level 0] Title: A 강조하기, Time: 0.08 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<myString.length(); i++){
+            if(myString.charAt(i)=='a' || myString.charAt(i)=='A'){
+                sb.append('A');
+            }else{
+                sb.append(Character.toLowerCase(myString.charAt(i)));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
