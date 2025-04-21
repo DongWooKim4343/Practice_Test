5c2cd449fe480dafe49656fa0f09c0db356d6e36
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-04-21 23:27:06 +0900

    [Bronze IV] Title: 새천년관, Time: 92 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/33701. 새천년관/새천년관.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+ 
+ class Main {
+     public static void main(String[] args) throws IOException {
+         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+         
+         bw.write("nukugwan");
+         bw.flush();
+         bw.close();
+            
+         
+         
+     }
+ }
