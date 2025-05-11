6aa3ed34bcda5bd55fb196484f93d7cb80c02ad3
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-05-11 18:18:27 +0900

    [Gold IV] Title: Jug Hard, Time: 384 ms, Memory: 44260 KB -BaekjoonHub

---
+++ 백준/Gold/11311. Jug Hard/Jug Hard.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            int gcd = getGcd(a, b);
+
+            if (c % gcd == 0) {
+                sb.append("Yes").append("\n");
+            } else {
+                sb.append("No").append("\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+
+}
+    static int getGcd(int a, int b){
+        if(b==0){
+            return a;
+        }
+        return getGcd(b, a%b);
+    }
+}
