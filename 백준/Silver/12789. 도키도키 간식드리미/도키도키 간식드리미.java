7f65ea174b1ad796ffedb95e83759a43f7d6f8ef
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-05-15 17:57:49 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 112 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,54 +1,49 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
-        Stack<Integer> stackVer = new Stack<>();
-        Stack<Integer> stackHor = new Stack<>();
-        List<Integer> list = new ArrayList<>();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
-            list.add(Integer.parseInt(st.nextToken()));
-        }
-        for(int i = 0; i < N; i++){
-            stackHor.push(list.get(N-1-i));
+
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        for(int i=0; i<N; i++){
+            queue.offer(Integer.parseInt(st.nextToken()));
         }
 
-        int number = 1;
+        Stack<Integer> stack = new Stack<>();
 
-        while(number <= N){
-            if(!stackVer.isEmpty() && stackVer.peek() == number){
-                stackVer.pop();
-                number++;
-            }
-            else if(!stackHor.isEmpty() && stackHor.peek() == number){
-                stackHor.pop();
-                number++;
-            }
-            else if (!stackHor.isEmpty()){ 
-                stackVer.push(stackHor.pop());
-            }
-            else{
+        int target = 1;
+
+        while(target <= N){
+            if(!queue.isEmpty() && queue.peek()==target){
+                queue.poll();
+                target++;
+            }else if(!stack.isEmpty() && stack.peek()==target){
+                stack.pop();
+                target++;
+            }else if(!queue.isEmpty()){
+                stack.push(queue.poll());
+            }else{
                 break;
             }
-        }
 
-        if(number>N){
+        }
+        if(target > N){
             bw.write("Nice");
         }else{
             bw.write("Sad");
         }
 
         bw.flush();
-        br.close();
         bw.close();
-
+        br.close();
 
 
     }
