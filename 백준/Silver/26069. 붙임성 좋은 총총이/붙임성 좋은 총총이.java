a01764f431fb42c92f16c446a225073c1b30f49b
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-05-27 12:06:03 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 116 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -8,35 +8,21 @@ class Main{
 
         int N = Integer.parseInt(br.readLine());
 
-        Map<String, Boolean> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        set.add("ChongChong");
 
         for(int i=0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             String left = st.nextToken();
             String right = st.nextToken();
 
-            if(left.equals("ChongChong") || right.equals("ChongChong")){
-                map.put(left, true);
-                map.put(right, true);
-            }else if(map.getOrDefault(left, false) || map.getOrDefault(right,false)){
-                map.put(left, true);
-                map.put(right, true);
-            }else{
-                map.put(left, map.getOrDefault(left, false));
-                map.put(right, map.getOrDefault(right, false));
+            if(set.contains(left) || set.contains(right)){
+                set.add(left);
+                set.add(right);
             }
         }
 
-        Set<Map.Entry<String,Boolean>> entry = map.entrySet();
-
-        int count = 0;
-        for(Map.Entry<String,Boolean> pp: entry){
-            if(pp.getValue()){
-                count++;
-            }
-        }
-
-        bw.write(count+"");
+        bw.write(set.size()+"");
         bw.flush();
         bw.close();
         br.close();
