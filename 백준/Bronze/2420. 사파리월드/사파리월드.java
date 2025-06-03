da3f58be1acb494ea7900857842ef3e0d3f44a40
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-06-03 13:05:12 +0900

    [Bronze V] Title: 사파리월드, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long N = Long.parseLong(st.nextToken());
+        long M = Long.parseLong(st.nextToken());
+
+        long answer = Math.abs(N-M);
+
+        bw.write(answer+"");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
