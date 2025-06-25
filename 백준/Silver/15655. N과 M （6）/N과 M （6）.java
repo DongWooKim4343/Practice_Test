1e6c45986d967988b1261b42a963ee4d479265be
DongWoo Kim <106728608+DongWooKim4343@users.noreply.github.com>
2025-06-25 16:04:36 +0900

    [Silver III] Title: N과 M (6), Time: 108 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    static boolean[] visit;
+    static int[] ansArr;
+    static int N, M;
+    static int[] numArr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        numArr = new int[N];
+        ansArr = new int[M];
+        visit = new boolean[N+1];
+
+        for(int i=0; i<N; i++){
+            numArr[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numArr);
+
+        backtracking(0, 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    static void backtracking(int depth, int start){
+        if(depth == M){
+            for(int num : ansArr){
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start; i<N; i++ ){
+            int n = numArr[i];
+
+            if(!visit[i]){
+                visit[i]=true;
+                ansArr[depth] = n;
+                backtracking(depth+1, i+1);
+                visit[i]=false;
+            }
+
+        }
+    }
+}
